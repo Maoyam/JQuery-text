c791a8285d15f8f5f2eff78bdf8968c81c100567
M.Aoyama <maruuca.com@gmail.com>
2023-12-05 08:10:20 +0900

    remove

--- js/dom.js
+++ js/dom.js
@@ -5,4 +5,15 @@ $(function() {
   num++;
   $('ul').append('<li>appendで追加' + num + '</li>');
  });
+
+ $('#remove').on('click', function() {
+  // 追加した分だけ削除するための準備
+  num--;
+  $('li:last').remove();
+
+  要素がなくなったら変数を0にする
+  if(num < 0){
+    num = 0;
+  }
+ });
 });
