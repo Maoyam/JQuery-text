a58f9bdf8c9a3ee4620392e944b6b2d9542706eb
M.Aoyama <maruuca.com@gmail.com>
2023-12-04 23:21:21 +0900

    toggleClass

--- js/class-operation.js
+++ js/class-operation.js
@@ -1,11 +1,6 @@
 $(function() {
   // ボタンをクリック
-  $('#add').on('click', function(){
-    // 指定要素にclassを追加
-    $('#target').addClass('sample');
-  });
-
-  $('#remove').on('click', function(){
-    $('#target').removeClass('sample');
+  $('#toggle').on('click', function(){
+    $('#target').toggleClass('sample');
   });
 });
